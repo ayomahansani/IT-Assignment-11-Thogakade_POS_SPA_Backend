c8f747eb6073dada9e4f41431cec5e40c7845086
ayomahansani <ayohansi17@gmail.com>
2024-05-15 10:37:33 +0530

    created customerModel getters and setters

--- model/customerModel.js
+++ model/customerModel.js
@@ -1,9 +1,41 @@
 export class CustomerModel {
 
     constructor(id, name, address, phone) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-        this.phone = phone;
+        this._id = id;
+        this._name = name;
+        this._address = address;
+        this._phone = phone;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    set id(id) {
+        this._id = id;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(name) {
+        this._name = name;
+    }
+
+    get address() {
+        return this._address;
+    }
+
+    set address(address) {
+        this._address = address;
+    }
+
+    get phone() {
+        return this._phone;
+    }
+
+    set phone(phone) {
+        this._phone = phone;
     }
 }
