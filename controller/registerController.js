75e1475e355a806a576422b08eb10628b53ef8d5
ayomahansani <ayohansi17@gmail.com>
2024-05-19 15:07:05 +0530

    imported class and array to the registerController.js

---
+++ controller/registerController.js
@@ -0,0 +1,3 @@
+// import class
+import {RegisterModel} from "../model/registerModel";
+
---
+++ model/registerModel.js
@@ -0,0 +1,43 @@
+
+export class RegisterModel {
+
+    constructor(firstName, lastName, username, password) {
+        this._firstName = firstName;
+        this._lastName = lastName;
+        this._username = username;
+        this._password = password;
+    }
+
+
+    get firstName() {
+        return this._firstName;
+    }
+
+    set firstName(firstName) {
+        this._firstName = firstName;
+    }
+
+    get lastName() {
+        return this._lastName;
+    }
+
+    set lastName(lastName) {
+        this._lastName = lastName;
+    }
+
+    get username() {
+        return this._username;
+    }
+
+    set username(username) {
+        this._username = username;
+    }
+
+    get password() {
+        return this._password;
+    }
+
+    set password(password) {
+        this._password = password;
+    }
+}
